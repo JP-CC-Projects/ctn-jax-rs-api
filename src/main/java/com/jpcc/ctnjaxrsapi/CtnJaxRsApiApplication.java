37047482b228ba72c566491f514947f83e45934e
JP-CC-Projects <joeparkcc@gmail.com>
2024-05-05 07:52:35 -0500

    mocking project with spring so that it can be delpoyed to railway

---
+++ src/main/java/com/jpcc/ctnjaxrsapi/CtnJaxRsApiApplication.java
@@ -0,0 +1,13 @@
+package com.jpcc.ctnjaxrsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtnJaxRsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtnJaxRsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpcc/ctnjaxrsapi/CtnJaxRsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpcc.ctnjaxrsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtnJaxRsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
