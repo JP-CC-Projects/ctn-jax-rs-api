08433deeb2c2bac6579b761a70af3f352ec29ac6
JP-CC-Projects <joeparkcc@gmail.com>
2024-05-06 20:01:09 -0500

    Updated mirrored repository with latest changes

--- src/main/java/org/ccode/asset/ctn/Main.java
+++ src/main/java/org/ccode/asset/ctn/Main.java
@@ -2,10 +2,17 @@ package org.ccode.asset.ctn;
 
 import org.apache.catalina.startup.Tomcat;
 
+
 public class Main {
     public static void main(String[] args) throws Exception {
         Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8080);
+
+        String webPort = System.getenv("PORT");
+        if (webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+
+        tomcat.setPort(Integer.parseInt(webPort));
 
         tomcat.getConnector();
         tomcat.addWebapp("", new java.io.File("src/main/webapp").getAbsolutePath());
