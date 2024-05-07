60d0042d21e42bc3ccba9ba1493a415f9630dad6
JP-CC-Projects <joeparkcc@gmail.com>
2024-05-06 20:21:14 -0500

    Updated mirrored repository with latest changes

--- src/main/java/org/ccode/asset/ctn/Main.java
+++ src/main/java/org/ccode/asset/ctn/Main.java
@@ -1,16 +1,21 @@
 package org.ccode.asset.ctn;
-
 import org.apache.catalina.startup.Tomcat;
-
-
 public class Main {
     public static void main(String[] args) throws Exception {
         Tomcat tomcat = new Tomcat();
+        System.out.println("The Main class is running - 1");
+        System.out.println("The Main class is running - 2");
+        System.out.println("The Main class is running - 3");
+        System.out.println("The Main class is running - 4");
+        System.out.println("The Main class is running - 5");
+
 
         String webPort = System.getenv("PORT");
         if (webPort == null || webPort.isEmpty()) {
             webPort = "8080";
         }
+
+        System.out.println("ABC: " + webPort);
 
         tomcat.setPort(Integer.parseInt(webPort));
 
