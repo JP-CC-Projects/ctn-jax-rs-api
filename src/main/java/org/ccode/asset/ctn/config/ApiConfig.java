ba64d96e48da1e0830cfe987ccf0c34eb44462cb
JP-CC-Projects <joeparkcc@gmail.com>
2024-08-16 00:08:34 -0500

    ensuring services are injectable

--- src/main/java/org/ccode/asset/ctn/config/ApiConfig.java
+++ src/main/java/org/ccode/asset/ctn/config/ApiConfig.java
@@ -1,4 +1,6 @@
 package org.ccode.asset.ctn.config;
+import org.ccode.asset.ctn.service.OperationService;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.jackson.JacksonFeature;
 
 import org.glassfish.jersey.server.ResourceConfig;
@@ -6,6 +8,12 @@ import org.glassfish.jersey.server.ResourceConfig;
 public class ApiConfig extends ResourceConfig {
     public ApiConfig(){
         register(JacksonFeature.class);
-
+        packages("org.ccode.asset.ctn.rest");
+        register(new AbstractBinder() {
+            @Override
+            protected void configure() {
+                bind(OperationService.class).to(OperationService.class);
+            }
+        });
     }
 }
